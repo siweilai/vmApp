a12fafc7864e8b2ebb01bc5873a865ea723be2be
zwm1114 <zwm1114@163.com>
2019-09-06 20:38:48 +0800

    添加登陆功能

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -5,11 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    url:'',
+    name:'',
+    voluem:'',
+    charge:0,
   },
 
   finish: function () {
-    wx: wx.navigateTo({
+    wx.navigateTo({
       url: '/pages/pay/success',
       success: function (res) { },
       fail: function (res) { },
@@ -21,6 +24,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // console.log(options.id)
+    var that = this;
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5d7204e7c4321b0682150364/demo/',
+      data:{
+        id:options.id
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success:function(res){
+        console.log("res",res)
+          that.setData({
+            name:res.data.data.name,
+            url:res.data.data.img
+          })
+      }
+    })
 
   },
 
--- pages/person/person.js
+++ pages/person/person.js
@@ -5,14 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {}
   },
-
+  
+  getUserInfo: function (e) {
+    var that = this;
+    if (e.detail.userInfo) {
+      wx.setStorageSync('userInfo', e.detail.userInfo)
+      that.setData({
+        userInfo: e.detail.rawData
+      })
+      var name = wx.getStorageSync('userInfo')
+      var superiorId = wx.getStorageSync('superiorId')
+      // 登录
+      if (wx.getStorageSync('userInfo')) {
+        wx.setStorageSync('isFirst', 'true')
+        setTimeout(function () {
+          wx.switchTab({
+            url: '/pages/index/index'
+          })
+        }, 1000)
+      }
+    } else {
+      wx.showToast({
+        title: "为了您更好的体验,请先同意授权",
+        icon: 'none',
+        duration: 2000
+      });
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const that = this;
+    that.setData({
+      avatarUrl: wx.getStorageSync('userInfo').avatarUrl,
+      nickName: wx.getStorageSync('userInfo').nickName
+    })
   },
 
   /**
